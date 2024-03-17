e6fea9f1b33890d93690b6d91460c708456ae828
Linucx4YOU3 <158158782+LinucxMD@users.noreply.github.com>
2024-03-17 23:49:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
   discordToken: 'TOKEN_DISCORD_BOT_ANDA',
   pterodactylApiKey: 'API_KEY_PTERODACTYL_ANDA',
-  baseUrl: 'https://panel.yourpterodactyl.com/api/client',
-  cpu: 1,
-  ram: 512,
-  disk: 5120,
-  maxAllocations: 5,
+  baseUrl: 'https://lpanel.zanixon.xyz/api/client',
+  cpu: 0,
+  ram: 0,
+  disk: 2048,
+  maxAllocations: 20,
   maxNodes: 3,
   nodeId: 5, // ID node yang ingin digunakan untuk pembuatan server
   swap: -1 // Nilai swap
