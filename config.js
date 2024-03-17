39e4b11cf54ea2a391ed1afc71cdfb1a09bda9a2
Linucx4YOU3 <158158782+LinucxMD@users.noreply.github.com>
2024-03-17 23:46:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,9 @@ module.exports = {
   baseUrl: 'https://panel.yourpterodactyl.com/api/client',
   cpu: 1,
   ram: 512,
-  disk: 5120
+  disk: 5120,
+  maxAllocations: 5,
+  maxNodes: 3,
+  nodeId: 5, // ID node yang ingin digunakan untuk pembuatan server
+  swap: -1 // Nilai swap
 };
