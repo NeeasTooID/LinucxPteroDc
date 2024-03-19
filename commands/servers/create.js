950046b8a92a14d1aca73789a83512f6fb78f58a
ユスプカクウ <156079487+HindiaFtNpc@users.noreply.github.com>
2024-03-19 09:25:57 +0800

    update create.js

--- commands/servers/create.js
+++ commands/servers/create.js
@@ -15,7 +15,7 @@ module.exports = {
             "nest": 5,
             "egg": 18,
             "docker_image": "ghcr.io/parkervcp/yolks:nodejs_20",
-            "startup": "java -Xms128M -Xmx{{SERVER_MEMORY}}M -Dterminal.jline=false -Dterminal.ansi=true -jar {{SERVER_JARFILE}}",
+            "startup": "if [[ -d .git ]] && [[ {{AUTO_UPDATE}} == "1" ]]; then git pull; fi; if [[ ! -z ${NODE_PACKAGES} ]]; then /usr/local/bin/npm install ${NODE_PACKAGES}; fi; if [[ ! -z ${UNNODE_PACKAGES} ]]; then /usr/local/bin/npm uninstall ${UNNODE_PACKAGES}; fi; if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; if [[ "${MAIN_FILE}" == "*.js" ]]; then /usr/local/bin/node "/home/container/${MAIN_FILE}" ${NODE_ARGS}; else /usr/local/bin/ts-node --esm "/home/container/${MAIN_FILE}" ${NODE_ARGS}; fi",
             "limits": {
                 "memory": 2048,
                 "swap": 0,
