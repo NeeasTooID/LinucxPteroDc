c8529d984f7c6c584f3a5d2ac6aeca654511df19
ユスプカクウ <156079487+HindiaFtNpc@users.noreply.github.com>
2024-03-21 06:38:57 +0700

    Delete commands directory

--- commands/help.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'help',
-    description: 'Displays available commands.',
-    execute(message, args) {
-        message.channel.send('List of available commands: ...');
-        // Your logic to display help message
-    },
-};
